3a01a5dc6a2cdef7626fd741ac4c04fcbf4676f3
djdt <10266332+djdt@users.noreply.github.com>
2022-01-04 13:48:20 +1100

    include all used cpp source files

--- setup.py
+++ setup.py
@@ -5,10 +5,23 @@ import numpy as np
 with open("README.md") as fp:
     long_description = fp.read()
 
+sources = [
+    "ckwrap/_ckwrap.pyx",
+    "Ckmeans.1d.dp/src/dynamic_prog.cpp",
+    "Ckmeans.1d.dp/src/EWL2_dynamic_prog.cpp",
+    "Ckmeans.1d.dp/src/EWL2_fill_log_linear.cpp",
+    "Ckmeans.1d.dp/src/EWL2_fill_quadratic.cpp",
+    "Ckmeans.1d.dp/src/EWL2_fill_SMAWK.cpp",
+    "Ckmeans.1d.dp/src/fill_log_linear.cpp",
+    "Ckmeans.1d.dp/src/fill_quadratic.cpp",
+    "Ckmeans.1d.dp/src/fill_SMAWK.cpp",
+    "Ckmeans.1d.dp/src/select_levels.cpp",
+    "Ckmeans.1d.dp/src/weighted_select_levels.cpp",
+]
 
 ckwrap = Extension(
     name="_ckwrap",
-    sources=["ckwrap/_ckwrap.pyx"],
+    sources=sources,
     language="c++",
     include_dirs=["Ckmeans.1d.dp/src", np.get_include()],
     extra_compile_args=["-std=c++11", "-g0"],
