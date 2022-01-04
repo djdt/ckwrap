9a00fdc500204c914627ce188305398cc93b1cd4
djdt <10266332+djdt@users.noreply.github.com>
2022-01-04 13:42:29 +1100

    include src files in manifest for building without wheel

--- setup.py
+++ setup.py
@@ -6,27 +6,14 @@ with open("README.md") as fp:
     long_description = fp.read()
 
 
-sources = [
-    "ckwrap/_ckwrap.pyx",
-    "Ckmeans.1d.dp/src/dynamic_prog.cpp",
-    "Ckmeans.1d.dp/src/EWL2_dynamic_prog.cpp",
-    "Ckmeans.1d.dp/src/EWL2_fill_log_linear.cpp",
-    "Ckmeans.1d.dp/src/EWL2_fill_quadratic.cpp",
-    "Ckmeans.1d.dp/src/EWL2_fill_SMAWK.cpp",
-    "Ckmeans.1d.dp/src/fill_log_linear.cpp",
-    "Ckmeans.1d.dp/src/fill_quadratic.cpp",
-    "Ckmeans.1d.dp/src/fill_SMAWK.cpp",
-    "Ckmeans.1d.dp/src/select_levels.cpp",
-    "Ckmeans.1d.dp/src/weighted_select_levels.cpp",
-]
-
 ckwrap = Extension(
     name="_ckwrap",
-    sources=sources,
+    sources=["ckwrap/_ckwrap.pyx"],
     language="c++",
     include_dirs=["Ckmeans.1d.dp/src", np.get_include()],
     extra_compile_args=["-std=c++11", "-g0"],
 )
+
 
 setup(
     name="ckwrap",
@@ -38,7 +25,7 @@ setup(
     license="LGPL",
     url="https://github.com/djdt/ckwrap",
     packages=["ckwrap"],
-    package_data={"ckwrap": ["*.pyx", "*.pdx"]},
+    package_data={"ckwrap": ["*.pxd"]},
     ext_modules=cythonize(ckwrap),
     install_requires=["numpy", "Cython"],
     tests_require=["pytest", "scipy"],
